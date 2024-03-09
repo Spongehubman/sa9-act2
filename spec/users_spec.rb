4c2cce189ee2ec2a84a3e28e1c9430f785b426e9
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-08 22:17:06 -0600

    In 'spec/users_spec.rb', the 'before' block and the code inside that block has been commented out, and the 'be' statement in the third 'expect' statement was changed to an 'eql' statement. Now all three tests in 'spec/users_spec.rb' pass.

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -3,10 +3,11 @@ require 'users'
 RSpec.describe User do
     let(:user) { User.new("EngineerElephant") }
 
-    before do
+    # This is an unused "before" block for testing.
+    #before do
         # Setting a variable for later use.
-        given_name = user.username
-    end
+        #given_name = user.username
+    #end
 
     describe "#log_in" do
         it "logs the user in" do
@@ -25,7 +26,7 @@ RSpec.describe User do
 
     describe "#username" do
         it "returns the correct username" do
-            expect(user.username).to equal(given_name)
+            expect(user.username).to eql("EngineerElephant")
         end
     end
 end
