83424505ff8f3967dbb0fde008b3080819e8b196
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-08 22:10:07 -0600

    Added a third test to 'spec/users_spec.rb', which unfortunately fails currently. Also, work began on 'lib/todo.rb' (which was finished) and 'string_wrapping.rb'.

---
+++ lib/string_wrapping.rb
@@ -0,0 +1,5 @@
+class StringWrapper
+    def initialize(str)
+        @str = str
+    end
+end
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -3,6 +3,11 @@ require 'users'
 RSpec.describe User do
     let(:user) { User.new("EngineerElephant") }
 
+    before do
+        # Setting a variable for later use.
+        given_name = user.username
+    end
+
     describe "#log_in" do
         it "logs the user in" do
             user.log_in
@@ -15,6 +20,12 @@ RSpec.describe User do
             user.log_in
             user.log_out
             expect(user.logged_in).to be false
+        end
+    end
+
+    describe "#username" do
+        it "returns the correct username" do
+            expect(user.username).to equal(given_name)
         end
     end
 end
